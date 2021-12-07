c508d67ebcd7cdce6d042d69adf967d9bfa32841
Lakshay <lakshayyadav2006@gmail.com>
2021-12-07 14:57:41 +0530

    Added docstrings to the methods.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 
 
 def calculate_score(cards):
+    """Returns the sum of all the cards"""
     if (sum(cards) == 21) and (len(cards) == 2):
         return 0
 
@@ -12,11 +13,13 @@ def calculate_score(cards):
 
 
 def deal_card():
+    """Returns a random card"""
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     return random.choice(cards)
 
 
 def compare(user_score, computer_score):
+    """Compares the scores and returns the result."""
     if user_score == computer_score:
         return "Draw."
     elif computer_score == 0:
@@ -36,6 +39,8 @@ def compare(user_score, computer_score):
 def start_blackjack():
     ai_cards = []
     player_cards = []
+    
+    # deal 2 cards to both the player and the AI
     for _ in range(2):
         player_cards.append(deal_card())
         ai_cards.append(deal_card())
